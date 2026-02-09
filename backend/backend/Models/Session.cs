a56a7d91e03b20f38037d3c1f45f32ce2f0cfb9b
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-09 14:55:20 +0100

    added SessionHash attribute

--- backend/backend/Models/Session.cs
+++ backend/backend/Models/Session.cs
@@ -1,9 +1,10 @@
 ﻿namespace backend.Models;
 
-public class Session(int sessionId, DateTime creationDate, DateTime expiryDate, int userId)
+public class Session(int sessionId, DateTime creationDate, DateTime expiryDate, int userId, string sessionHash)
 {
     public int SessionId { get; set; } = sessionId;
     public DateTime CreationDate { get; set; } = creationDate;
     public DateTime ExpiryDate { get; set; } = expiryDate;
     public int UserId { get; set; } = userId;
+    public string SessionHash { get; set; } = sessionHash;
 }
