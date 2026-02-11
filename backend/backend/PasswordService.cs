dcc9d78926e2d8cc4fa74fd9c6d8c3e43411a9c3
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-11 10:46:24 +0100

    Create Passwordhasher

---
+++ backend/backend/PasswordService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace backend;
+
+public static class PasswordService
+{
+    private static readonly PasswordHasher<string> hasher = new();
+    
+    public static string HashPassword(string pw)
+    {
+        return hasher.HashPassword(null, pw);
+    }
+
+    public static bool VerifyPassword(string hashedPw, string providedPw)
+    {
+        var result = hasher.VerifyHashedPassword(null, hashedPw, providedPw);
+        return result == PasswordVerificationResult.Success;
+    }
+}
