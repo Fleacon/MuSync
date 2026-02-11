63b9986beaa220bfda49ca392c5e8d11e598ca58
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-12 00:13:10 +0100

    Implemented Register Endpoint

--- backend/backend/Controllers/AccountController.cs
+++ backend/backend/Controllers/AccountController.cs
@@ -38,5 +38,20 @@ public class AccountController : ControllerBase
         
         var providerAccesses = await tokenManager.GenerateProviderAccess(user); 
         return Ok(new SessionContext(username, providerAccesses));
+    [HttpPost("Register")]
+    public async Task<ActionResult<SessionContext>> TryRegister([FromBody] UserAuthData userAuthData)
+    {
+        var user = await usersDao.GetUserByUsername(userAuthData.Username);
+        if (user is not null)
+            return Conflict();
+
+        string username = userAuthData.Username;
+        string hashedPw = PasswordService.HashPassword(userAuthData.Password);
+
+        var newUser = await usersDao.CreateUser(new(0, username, hashedPw));
+
+        await sessionManager.GenerateSession(Response, newUser.UserId);
+        
+        return Ok(new SessionContext(newUser.Username, null));
     }
 }
