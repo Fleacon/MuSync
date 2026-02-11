7901a18796137593dbeb1bf1227ac4a54a114738
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-11 10:47:19 +0100

    Creatae class for generating AccessTokens

---
+++ backend/backend/TokenManager.cs
@@ -0,0 +1,32 @@
+﻿using backend.DB.DAO;
+using backend.Models;
+
+namespace backend;
+
+public class TokenManager
+{
+    private OAuthTokensDAO tokensDao;
+    
+    public TokenManager(OAuthTokensDAO tokensDao)
+    {
+        this.tokensDao = tokensDao;
+    }
+    
+    public async Task<List<ProviderAccess>> GenerateProviderAccess(User u)
+    {
+        var providerAccess = new List<ProviderAccess>();
+        var tokens = await tokensDao.GetOAuthTokenByUserId(u.UserId);
+        foreach (var t in tokens)
+        {
+            string accessToken = await GenerateNewAccessToken(t);
+            providerAccess.Add(new ProviderAccess(t.Provider, accessToken));
+        }
+
+        return providerAccess;
+    }
+
+    public async Task<string> GenerateNewAccessToken(OAuthToken oAuthToken)
+    {
+        return "dummy";
+    }
+}
