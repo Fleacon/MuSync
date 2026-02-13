5ad67ba93dff126036298dbe142e814f6b42a4fb
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-13 10:31:41 +0100

    Update AccountPage.vue

--- frontend/src/AccountPage.vue
+++ frontend/src/AccountPage.vue
@@ -1,1 +1,16 @@
-<template></template>
+<script setup>
+import Cookies from 'js-cookie'
+import { onMounted, ref } from 'vue'
+
+function logout() {
+  Cookies.remove('Session')
+  fetch('http://localhost:5123/api/Account/Logout', {
+    method: 'POST',
+    credentials: 'include',
+  })
+}
+</script>
+
+<template>
+  <button @click="logout">Logout</button>
+</template>
