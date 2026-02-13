0458847a172ac2bd9f3256a437758cd7e8141ad4
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-13 11:31:48 +0100

    remove provider cookie, user data from localstorage

--- frontend/src/AccountPage.vue
+++ frontend/src/AccountPage.vue
@@ -4,10 +4,14 @@ import { onMounted, ref } from 'vue'
 
 function logout() {
   Cookies.remove('Session')
+  Cookies.remove('ProviderList')
   fetch('http://localhost:5123/api/Account/Logout', {
     method: 'POST',
     credentials: 'include',
   })
+  localStorage.removeItem('username')
+  localStorage.removeItem('providerList')
+  window.dispatchEvent(new CustomEvent('logout-success'))
 }
 </script>
 
