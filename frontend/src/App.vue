54701423a4df016bc1ffaec774e8f3c0d6d945df
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-14 16:35:17 +0100

    moved css variables to own file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -50,15 +50,6 @@ onUnmounted(() => {
 </template>
 
 <style>
-:root {
-  --primary-color: #0c2b4e;
-  --secondary-color: #1a3d64;
-  --accent1-color: #1d546c;
-  --accent2-color: #f4f4f4;
-  --background-linear1-color: var(--primary-color);
-  --background-linear2-color: #0b2435;
-}
-
 html {
   height: 100%;
 }
