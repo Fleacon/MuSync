f4541ffd84bfed1d726686b5579bd0f4a3b74b3e
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-14 16:36:01 +0100

    create page for provider authentication

--- frontend/src/AccountPage.vue
+++ frontend/src/AccountPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Cookies from 'js-cookie'
 import { onMounted, ref } from 'vue'
+import ProviderAuth from './components/ProviderAuth.vue'
 
 function logout() {
   Cookies.remove('Session')
@@ -17,4 +18,19 @@ function logout() {
 
 <template>
   <button @click="logout">Logout</button>
+  <div class="accountPage">
+    <ProviderAuth provider="YouTube Music" iconClass="fa-brands fa-youtube" />
+    <ProviderAuth provider="Spotify" iconClass="fa-brands fa-spotify" />
+    <ProviderAuth provider="SoundCloud" iconClass="fa-brands fa-soundcloud" />
+  </div>
 </template>
+
+<style>
+.accountPage {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 80%;
+  margin: auto;
+}
+</style>
