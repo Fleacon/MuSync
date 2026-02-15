e59b8a82003935edb3281055c662d71ac1fe0c4e
Fleacon <146029061+Fleacon@users.noreply.github.com>
2026-02-15 16:36:01 +0100

    added endpoints for validating sessions

---
+++ backend/backend/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using backend.DB.DAO;
+using backend.Models;
+using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private SessionsDAO sessionsDao;
+    private UsersDAO usersDao;
+    private OAuthTokensDAO authTokensDao;
+    private SessionService sessionService;
+
+    public AuthController(SessionsDAO sessionsDao, UsersDAO usersDao, OAuthTokensDAO authTokensDao, SessionService sessionService)
+    {
+        this.sessionsDao = sessionsDao;
+        this.usersDao = usersDao;
+        this.authTokensDao = authTokensDao;
+        this.sessionService = sessionService;
+    }
+    
+    [HttpGet("Me")]
+    public async Task<ActionResult<SessionContext>> GetSessionContext()
+    {
+        if (!Request.Cookies.TryGetValue("Session", out var token))
+            return Unauthorized();
+        var user = await usersDao.GetUserByHashedSessionToken(sessionService.HashSessionToken(token));
+        if (user is null)
+            return Unauthorized();
+        var providers = await authTokensDao.GetOAuthTokenByUserId(user.UserId);
+        var providersList = providers
+            .Select(p => p.Provider)
+            .Distinct()
+            .ToList();
+
+        var context = new SessionContext(user.Username, providersList);
+        return Ok(context);
+    }
+}
